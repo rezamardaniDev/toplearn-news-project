4bc5e8b58c1c214d91ea8ead414b82a62d243f57
reza <mardanireza58@gmail.com>
2024-12-30 10:51:00 +0330

    add: helpers -> protocol,currentDomain,asset,url,currentUrl

--- index.php
+++ index.php
@@ -5,18 +5,41 @@ session_start();
 
 //initial config
 define('BASE_PATH', __DIR__);
-
 define('CURRENT_DOMAIN', currentDomain() . '/project/');
-
 define('DISPLAY_ERROR', true);
 
 // database config
 define('DB_HOST', 'localhost');
-
 define('DB_NAME', 'project');
-
 define('DB_USERNAME', 'root');
-
 define('DB_PASSWORD', '');
 
+// helpers
+function protocol()
+{
+    return stripos($_SERVER['SERVER_PROTOCOL'], 'https') === true ? 'https://' : 'http://';
+}
 
+function currentDomain()
+{
+    return protocol() . $_SERVER['HTTP_HOST'];
+}
+
+function asset(string $src)
+{
+    $domain = trim(CURRENT_DOMAIN, '/ ');
+    $src = $domain . '/' . trim($src, '/');
+    return $src;
+}
+
+function url(string $url)
+{
+    $domain = trim(CURRENT_DOMAIN, '/ ');
+    $src = $domain . '/' . trim($url, '/');
+    return $src;
+}
+
+function currentUrl()
+{
+    return currentDomain() . $_SERVER['REQUEST_URI'];
+}
