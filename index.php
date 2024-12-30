9c10783456f044725d68e64154cb62be3e37cc4c
reza <mardanireza58@gmail.com>
2024-12-30 11:54:15 +0330

    add: helpers -> methodField, displayError, flashMessage

--- index.php
+++ index.php
@@ -43,3 +43,41 @@ function currentUrl()
 {
     return currentDomain() . $_SERVER['REQUEST_URI'];
 }
+
+function methodField()
+{
+    return  $_SERVER['REQUEST_METHOD'];
+}
+
+function displayError($display_error)
+{
+    if ($display_error) {
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
+    } else {
+        ini_set('display_errors', 0);
+        ini_set('display_startup_errors', 0);
+        error_reporting(0);
+    }
+}
+displayError(DISPLAY_ERROR);
+
+global $flashMessage;
+if (isset($_SESSION['flas_message'])) {
+    $flashMessage = $_SESSION['flash_message'];
+    unset($_SESSION['flash_message']);
+}
+
+
+function flash($name, $value = null)
+{
+    if ($value === null) {
+        global $flashMessage;
+        $message = isset($_SESSION[$name]) ? $flashMessage[$name] : '';
+        return $message;
+    } else {
+        $_SESSION['flash_message'][$name] = $value;
+    }
+}
+    
